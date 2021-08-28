cc04ce62620c0c30948ea2ed79101a7a561d9dd5
RamazanOzmert <86482530+RamazanOzmert@users.noreply.github.com>
2021-08-28 19:46:56 +0300

    Update settings.py

--- ramazanozmert/settings.py
+++ ramazanozmert/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'ramazanozmert.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'darbnqsisct04f',
+        'USER': 'hwmuuslmjkcnpi',
+        'PASSWORD': '372bd47d77bb9fabf3257bf5a878b5c026d5685c28863fe76f72db79b0325919',
+        'HOST': 'ec2-54-147-93-73.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
