e31a5d956e054588e244a9785a052aa80d629026
RamazanOzmert <86482530+RamazanOzmert@users.noreply.github.com>
2021-08-29 12:46:48 +0300

    Update settings.py

--- ramazanozmert/settings.py
+++ ramazanozmert/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pr5d40+yn9ho9c6vfg_r6f(%3h!zi*qi5%ki^y=to5)-1#gw('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ramazanozmert.herokuapp.com','127.0.0.1']
 
